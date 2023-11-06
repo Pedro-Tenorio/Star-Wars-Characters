bd561dae7a1cf63be8252435069d5fed56bc77a9
Pedro Tenorio <pearthurte@gmail.com>
2023-11-06 18:46:55 -0300

    integração js

--- js/script.js
+++ js/script.js
@@ -7,5 +7,46 @@ window.onload = async () => {
         console.log(error);
         alert('Erro ao carregar cards');
     }
+
+    const nextButton = document.getElementById('next-button')
+    const backButton = document.getElementById('back-button')
+
+    nextButton.addEventListener(click, loadNextPage)
+    backButton.addEventListener(click, loadPreviousPage)
+    
 }
 
+async function loadCharacters(url) {
+    const mainContent = document.getElementById('main-content')
+    mainContent.innerHTML = '';
+    try {
+        const response = await fetch(url);
+
+        const responseJson = await response.json();
+
+        for(character in responseJson.results) {
+            const contain = responseJson.results[character];
+            const characterName = 
+            `<div class="cards" style="background-image: url('https://starwars-visualguide.com/assets/img/characters/${contain.url.replace(/\D/g, "")}.jpg');">                
+                <div class="character-name-bg">
+                    <span class="character-name">${contain.name}</span>
+                </div>
+            </div>`
+
+            document.querySelector('#main-content').innerHTML += characterName
+        }
+
+        const nextButton = document.getElementById('next-button')
+        const backButton = document.getElementById('back-button')
+
+        nextButton.disable = !responseJson.next
+        backButton.disable = !responseJson.previous
+
+        backButton.style.visibility = response.Jasen.previous? "visible" : "hidden"
+
+        currentPageUrl = urls
+    } catch (error) {
+        alert('erro ao carregar os personagens')
+        console.log(error)
+    }
+}
